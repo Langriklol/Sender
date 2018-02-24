648794c916e301284d88922c5e6c44f073c55653
Ondřej Langr <lango@doomsdaymachine.DOMA>
2018-02-24 02:12:53 +0100

    Done :)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO.Ports;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Sender
 {
@@ -8,9 +9,32 @@ namespace Sender
     {
         static void Main(string[] args)
         {
+            string filePath = args[0];
+            
+            //Thanks to .NET Core and its prohibition to support System.IO.Ports only on 
+            //Windows these lines are commented and class useless
+            // --------------------
+            //string device = args[1];
+            //int baudrate = !String.IsNullOrEmpty(args[2]) ? int.Parse(args[2]) : 115200;
+            //Sender sender = new Sender(device, baudrate);
+            
             Parser parser = new Parser();
-            parser.Parse("./prelude.json");
-            Console.Write(parser.getJson());
+            parser.Parse(filePath);
+
+            foreach(Note note in parser.getTones())
+            {
+                //sender.Send(note); - Another one useless thing just because Windows is junk
+                Console.WriteLine(
+                String.Format(
+                    "{0},{1},{2}",
+                    note.getName(),
+                    note.getFrequency(), 
+                    note.getDuration()
+                )
+            );
+                Task.Delay((int)Math.Round(note.getDuration())).Wait();
+            }
+
         }
     }
 }
--- Sender.cs
+++ Sender.cs
@@ -1,7 +1,35 @@
+using System;
+using System.IO.Ports;
+
 namespace Sender{
+    //Honestly, you do not need this class... System.IO.Ports is working only under Windows... 
     public class Sender{
-        public Sender(){
-            
+
+        private SerialPort _serialPort;
+
+        /// <param name="device">Name of device - p.g. /dev/ttyUSB0 </param>
+        public Sender(string device, int baudRate){
+            this._serialPort = new SerialPort(device)
+            {
+                BaudRate = baudRate,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 8,
+                Handshake = Handshake.None
+            };
+        }
+
+        /// <param name="note">Note itself</param>
+        public void Send(Note note)
+        {
+            this._serialPort.WriteLine(
+                String.Format(
+                    "{0},{1},{2}",
+                    note.getName(),
+                    note.getFrequency(), 
+                    note.getDuration()
+                )
+            );
         }
     }
 }
