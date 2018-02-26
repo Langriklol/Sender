f1cdc1177772cadee49c8bb89a335190002d4a99
Langriklol <langrik666@gmail.com>
2018-02-26 14:40:18 +0100

    Bugfix v3.
    Portation to Windows... well, I hope that they will user Linux(Fedora) just because of Linux

--- Program.cs
+++ Program.cs
@@ -10,29 +10,41 @@ namespace Sender
         static void Main(string[] args)
         {
             string filePath = args[0];
-            
-            //Thanks to .NET Core and its prohibition to support System.IO.Ports only on 
-            //Windows these lines are commented and class useless
-            // --------------------
-            //string device = args[1];
-            //int baudrate = !String.IsNullOrEmpty(args[2]) ? int.Parse(args[2]) : 115200;
-            //Sender sender = new Sender(device, baudrate);
-            
             Parser parser = new Parser();
             parser.Parse(filePath);
-            
-            foreach(Note note in parser.getTones())
+
+            OperatingSystem sys = Environment.OSVersion;
+            PlatformID pid = sys.Platform;
+
+            string portation = args[1];
+            if (portation.ToLower() == "windows" || portation != "" || pid == PlatformID.WinCE)
             {
-                //sender.Send(note); - Another one useless thing just because Windows is junk
-                Console.WriteLine(
-                String.Format(
-                    "{0},{1},{2}",
-                    note.getName(),
-                    note.getFrequency(), 
-                    note.getDuration()
-                )
-            );
-                Task.Delay((int)Math.Round(note.getDuration())).Wait(); //Waiting when tone is playing - a bit messy
+                //Thanks to .NET Core and its prohibition to support System.IO.Ports only on 
+                //Windows these lines are commented and class useless if you are not on windows
+                // --------------------
+                string device = args[2] != "" ? args[2] : "COM1";
+                int baudrate = !String.IsNullOrEmpty(args[3]) ? int.Parse(args[3]) : 115200;
+                Sender sender = new Sender(device, baudrate);
+                foreach(Note note in parser.getTones())
+                {
+                    sender.Send(note);
+                    Task.Delay((int)Math.Round(note.getDuration())).Wait();
+                }
+            }
+            else
+            {
+                foreach (Note note in parser.getTones())
+                {
+                    Console.WriteLine(
+                    String.Format(
+                        "{0},{1},{2}",
+                        note.getName(),
+                        note.getFrequency(),
+                        note.getDuration()
+                    )
+                );
+                    Task.Delay((int)Math.Round(note.getDuration())).Wait(); //Waiting when tone is playing - a bit messy but still..
+                }
             }
         }
     }
